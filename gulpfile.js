91b2cb55f19c3bbb50ebf0df05eb41fb5852c6ad
kobaf <andre@dorax-invest.com>
2022-09-12 10:50:32 +0300

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('node-sass'));
-const watchSass = require("gulp-watch-sass");
-const browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+      sass = require('gulp-sass')(require('node-sass')),
+      watchSass = require("gulp-watch-sass"),
+      browserSync = require('browser-sync').create(),
+      del = require('del'),
+      imagemin = require('gulp-imagemin'),
+      uglify = require('gulp-uglify'),
+      usemin = require('gulp-usemin'),
+      rev = require('gulp-rev'),
+      cleanCss = require('gulp-clean-css'),
+      flatmap = require('gulp-flatmap'),
+      htmlmin = require('gulp-htmlmin');
 
 
  
@@ -34,7 +42,40 @@ gulp.task('sass', function () {
     });
  
  });
+
+ gulp.task('clean', function() {
+   return del(['dist']);
+ });
  
+ gulp.task('copyfonts', function() {
+   return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+   return gulp.src('img/*.{png,jpg,gif}')
+     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+     .pipe(gulp.dest('dist/img'));
+ });
+
+ gulp.task('usemin', function() {
+   return gulp.src('./*.html')
+   .pipe(flatmap(function(stream, file){
+       return stream
+         .pipe(usemin({
+             css: [ rev() ],
+             html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+             js: [ uglify(), rev() ],
+             inlinejs: [ uglify() ],
+             inlinecss: [ cleanCss(), 'concat' ]
+         }))
+     }))
+     .pipe(gulp.dest('dist/'));
+ });
+ 
+ gulp.task('build', gulp.series(['clean','copyfonts','imagemin','usemin']));
+
 
  
 gulp.task('default', gulp.parallel(['sass:watch','browser-sync']));
